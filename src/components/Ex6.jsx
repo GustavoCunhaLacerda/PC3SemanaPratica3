e227cf356628fa81fc0d42623a67a8d94a2b88b1
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2021-02-28 22:02:39 -0300

    senha

--- src/components/Ex6.jsx
+++ src/components/Ex6.jsx
@@ -3,8 +3,9 @@ import { TextField, Button } from '@material-ui/core';
 
 export default function Ex6() {
 
-    const [usuario, setUsuario] = useState();
-    const [senha, setSenha] = useState();
+    const [usuario, setUsuario] = useState('');
+    const [senha, setSenha] = useState('');
+    const [logado, setLogado] = useState(false);
 
     const inputStyle = {
         width: "220px",
@@ -13,53 +14,81 @@ export default function Ex6() {
     };
 
     const cadastrarUsuario = () => {
-        if(localStorage.getItem(usuario)) alert('Esse usuario já possui cadastrado');
+        if (localStorage.getItem(usuario)) alert('Esse usuario já possui cadastrado');
         else {
             localStorage.setItem(usuario, senha);
+            alert('Cadastrado com sucesso')
         }
 
-        console.log(localStorage)
     }
 
-    return (
-        <div>
+    const entrarComUsuario = () => {
+        if (localStorage.getItem(usuario) === senha) setLogado(true);
+        else alert('Usuário ou senha incorretos');
+    }
+
+    if (!logado) {
+
+        return (
             <div>
-                <TextField
-                    onChange={ e => setUsuario(e.target.value)}
-                    label="Usuário"
+
+                <div>
+                    <TextField
+                        id="usuario"
+                        onChange={e => setUsuario(e.target.value)}
+                        label="Usuário"
+                        variant="outlined"
+                        style={inputStyle}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="senha"
+                        type="password"
+                        onChange={e => setSenha(e.target.value)}
+                        label="Senha"
+                        variant="outlined"
+                        style={inputStyle}
+                    />
+                </div>
+
+                <Button
+                    onClick={() => entrarComUsuario()}
+                    variant="contained"
+                    size="medium"
+                    style={{
+                        backgroundColor: "#341f97",
+                        color: "#fff",
+                        marginRight: '20px'
+                    }}
+                >
+                    entrar
+                </Button>
+                <Button
+                    onClick={() => cadastrarUsuario()}
+                    size="medium"
                     variant="outlined"
-                    style={inputStyle}
-                />
+                >
+                    cadastrar
+                </Button>
             </div>
+        );
+    } else {
+        return (
             <div>
-                <TextField
-                    onClick={ e => setSenha(e.target.value)}
-                    label="Senha"
-                    variant="outlined"
-                    style={inputStyle}
-                />
+                <p>Logado com {usuario}</p>
+                <Button
+                    onClick={() => {setLogado(false); setSenha(''); setUsuario('');}}
+                    variant="contained"
+                    size="medium"
+                    style={{
+                        backgroundColor: "#341f97",
+                        color: "#fff",
+                    }}
+                >
+                    sair
+                </Button>
             </div>
-
-            <Button
-                variant="contained"
-                size="medium"
-                style={{
-                    backgroundColor: "#341f97",
-                    color: "#fff",
-                    marginRight: '20px'
-                }}
-            >
-                entrar
-            </Button>
-            <Button
-                onClick={cadastrarUsuario}
-                size="medium"
-                variant="outlined"
-            >
-                cadastrar
-            </Button>
-
-
-        </div>
-    );
+        );
+    }
 }
